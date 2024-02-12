ce8be1fcff644a999ac719dfeb6aa5072f8bad06
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-02-13 00:15:10 +0500

    bloc extensions installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_state_management/random.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,12 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-     
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: Random(),
     );
   }
 }
-
