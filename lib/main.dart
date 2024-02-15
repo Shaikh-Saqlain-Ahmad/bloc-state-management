c7b601d8f4b62556aba76c39671e9cc257c35095
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-02-15 12:41:39 +0500

    bloc packages installed and architecture setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_state_management/random.dart';
+import 'package:bloc_state_management/ui/counter-screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Random(),
+      home: CounterScreen(),
     );
   }
 }
