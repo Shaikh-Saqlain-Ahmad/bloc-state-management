b606af6ecfb5d4ab2aadb0bf817c8dc2ac1cfb86
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-02-15 13:14:49 +0500

    bloc states and events created

--- lib/ui/counter-screen.dart
+++ lib/ui/counter-screen.dart
@@ -10,6 +10,32 @@ class CounterScreen extends StatefulWidget {
 class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text("Counter Using Bloc"))),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                "0",
+                style: TextStyle(fontSize: 50),
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {}, child: const Text("Increment")),
+                const SizedBox(
+                  width: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text("Decrement")),
+              ],
+            )
+          ]),
+    );
   }
 }
