46b7bf17e355b57337e3d5657174ffb4c31e207f
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-02-17 14:16:57 +0500

    bloc logic implemented and main dart wrapped with bloc

--- lib/bloc/counter/counter-bloc.dart
+++ lib/bloc/counter/counter-bloc.dart
@@ -1,1 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:bloc_state_management/bloc/counter/counter-events.dart';
+import 'package:bloc_state_management/bloc/counter/counter-state.dart';
 
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(const CounterState()) {}
+  void increment(IncrementCounter event, Emitter<CounterState> emit) {
+    emit(state.copyWith(counter: state.counter + 1));
+  }
+
+  void decrement(IncrementCounter event, Emitter<CounterState> emit) {
+    emit(state.copyWith(counter: state.counter - 1));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_state_management/bloc/counter/counter-bloc.dart';
 import 'package:bloc_state_management/ui/counter-screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const CounterScreen(),
-    );
+    return BlocProvider(
+        create: (context) => CounterBloc(),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const CounterScreen(),
+        ));
   }
 }
